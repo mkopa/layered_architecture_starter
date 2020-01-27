709b760079b937c4b08859926380e319c97cfc05
kwasniew <kwasniewski.mateusz@gmail.com>
2020-01-27 15:52:37 +0100

    book service test preps

--- src/bookRoutes.js
+++ src/bookRoutes.js
@@ -3,6 +3,10 @@ const validateBookMiddleware = require("./validateBookMiddleware");
 const bookRepository = require("./bookRepository");
 const bookService = require("./bookService");
 const {createOrUpdate, getDetails} = require("./bookController")({bookService, bookRepository});
+// choose boring technology
+// compose, compose, compose
+// understand from first principles
+// priortize fast feedback
 
 router.post("/book", validateBookMiddleware, createOrUpdate);
 router.get("/book/:isbn", getDetails);
