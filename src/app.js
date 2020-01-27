806e17f9d13c06856c745093457bbba7ee27cdbc
kwasniew <kwasniewski.mateusz@gmail.com>
2020-01-27 12:01:02 +0100

    env

--- src/app.js
+++ src/app.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const app = express();
 const bookRoutes = require("./bookRoutes");
+const {notFound, error} = require("./error");
 
 app.use(express.json());
 app.use(bookRoutes);
 
-app.use(function notFound(req, res, next) {
-    const error = new Error("Not Found");
-    error.status = 404;
-    next(error);
-});
-
-app.use(function error(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({message: err.message, error: err.stack});
-});
+app.use(notFound);
+app.use(error);
 
 module.exports = app;
---
+++ src/error.js
@@ -0,0 +1,16 @@
+function notFound(req, res, next) {
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+}
+
+function error(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({message: err.message, error: isDevelopment() ? err.stack : {}});
+}
+
+function isDevelopment() {
+    return process.env.NODE_ENV === "development";
+}
+
+module.exports = {notFound, error};
