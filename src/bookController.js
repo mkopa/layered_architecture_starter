0fb77c5c4d8200ca989f106d2823f269252f49a1
kwasniew <kwasniewski.mateusz@gmail.com>
2020-01-27 12:06:20 +0100

    controller

---
+++ src/bookController.js
@@ -0,0 +1,37 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017/booksapi';
+
+let booksPromise = MongoClient.connect(url).then(function (client) {
+    return client.db().collection("books");
+});
+
+module.exports = {
+    async createOrUpdate(req, res, next) {
+        try {
+            const {title, authors, isbn, description} = req.body;
+
+            const books = await booksPromise;
+            await books.updateOne(
+                {isbn: isbn},
+                {$set: {title, authors, isbn, description}},
+                {upsert: true}
+            );
+
+            res.json({title, authors, isbn, description});
+        } catch (e) {
+            next(e);
+        }
+    },
+    async getDetails(req, res, next) {
+        try {
+            const isbn = req.params.isbn;
+
+            const books = await booksPromise;
+            const book = await books.findOne({isbn}, {projection: {_id: false}});
+            res.json(book);
+        } catch(e) {
+            next(e);
+        }
+    }
+};
--- src/bookRoutes.js
+++ src/bookRoutes.js
@@ -1,39 +1,7 @@
 const router = require('express').Router();
+const {createOrUpdate, getDetails} = require("./bookController");
 
-const MongoClient = require('mongodb').MongoClient;
-
-const url = 'mongodb://localhost:27017/booksapi';
-
-let booksPromise = MongoClient.connect(url).then(function (client) {
-    return client.db().collection("books");
-});
-
-router.post("/book", async function (req, res, next) {
-    try {
-        const {title, authors, isbn, description} = req.body;
-
-        const books = await booksPromise;
-        await books.updateOne(
-            {isbn: isbn},
-            {$set: {title, authors, isbn, description}},
-            {upsert: true}
-        );
-
-        res.json({title, authors, isbn, description});
-    } catch(e) {
-        next(e);
-    }
-});
-router.get("/book/:isbn", async function (req, res, next) {
-    try {
-        const isbn = req.params.isbn;
-
-        const books = await booksPromise;
-        const book = await books.findOne({isbn}, {projection: {_id: false}});
-        res.json(book);
-    } catch(e) {
-        next(e);
-    }
-});
+router.post("/book", createOrUpdate);
+router.get("/book/:isbn", getDetails);
 
 module.exports = router;
